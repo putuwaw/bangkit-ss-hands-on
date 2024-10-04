cfe4e99afe8c97cc670d6002bf29d184729649b2
Putu Widyantara <putuwaw973@gmail.com>
2024-10-04 23:12:49 +0800

    🚀feat: add error handling

--- app.py
+++ app.py
@@ -14,28 +14,39 @@ def hello_world():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    # get post data
-    data = request.get_json()
+    try:
+        # get post data
+        data = request.get_json()
 
-    # do something with data
-    data = np.array(data)
+        # do something with data
+        data = np.array(data)
 
-    # load model
-    model = keras.models.load_model("model/iris.keras")
+        # load model
+        model = keras.models.load_model("model/iris.keras")
 
-    class_name = ["setosa", "versicolor", "virginica"]
-    prediction = model.predict(data)
-    proba = float(np.max(prediction))
-    prediction = class_name[np.argmax(prediction)]
+        # get class names from iris.target_names
+        class_name = ["setosa", "versicolor", "virginica"]
+        prediction = model.predict(data)
+        proba = float(np.max(prediction))
+        prediction = class_name[np.argmax(prediction)]
 
-    return jsonify(
-        {
-            "status": True,
-            "code": 200,
-            "message": "OK",
-            "data": {"prediction": prediction, "probabilty": proba},
-        }
-    )
+        return jsonify(
+            {
+                "status": True,
+                "code": 200,
+                "message": "OK",
+                "data": {"prediction": prediction, "probabilty": proba},
+            }
+        )
+    except Exception as e:
+        return jsonify(
+            {
+                "status": False,
+                "code": 500,
+                "message": str(e),
+                "data": None,
+            }
+        ), 500
 
 
 if __name__ == "__main__":
